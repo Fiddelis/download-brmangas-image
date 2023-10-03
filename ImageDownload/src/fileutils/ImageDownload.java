21daa1abf430f422898c5cff3097296bd15444f3
fiddelis <lucas.ruan@ges.inatel.br>
2023-10-03 11:56:18 -0300

    Downloads das Imagens na Web

--- ImageDownload/src/fileutils/ImageDownload.java
+++ ImageDownload/src/fileutils/ImageDownload.java
@@ -1,39 +1,26 @@
-package fileutil;
+package fileutils;
+
+import webrequest.WebPageRequest;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URL;
-import java.net.URLConnection;
 
 public class ImageDownload {
-    public boolean downloadChapter(String imageUrl, String destinationFile, int chapter) throws IOException {
+    public boolean downloadPage(String imageUrl, String destinationFile, int chapter) throws IOException {
+        WebPageRequest webPageRequest = new WebPageRequest();
         InputStream inputStream;
 
-        // Criando conexão com o link .jpg/png
-        try {
-            URL url = new URL(imageUrl);
-            try {
-                URLConnection connection = url.openConnection();
-                inputStream = connection.getInputStream();
-            } catch (IOException e) {   // caso não consiga conexão com o link .jpg
-                imageUrl = imageUrl.substring(0, imageUrl.length() - 4) + ".png";    // altera o formato da imagem
+        try (FileOutputStream outputStream = new FileOutputStream(destinationFile)) {
+            byte[] buffer = new byte[4096];
+            int bytesRead;
 
-                url = new URL(imageUrl);
-                URLConnection connection = url.openConnection();
-                inputStream = connection.getInputStream();
+            inputStream = webPageRequest.inputRequest(imageUrl);
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, bytesRead);
             }
-
-            // Download da Imagem
-            try (FileOutputStream outputStream = new FileOutputStream(destinationFile)) {
-                byte[] buffer = new byte[4096];
-                int bytesRead;
-                while ((bytesRead = inputStream.read(buffer)) != -1) {
-                    outputStream.write(buffer, 0, bytesRead);
-                }
-                System.out.println(imageUrl);
-            }
+            System.out.println(imageUrl);
         } catch (FileNotFoundException e) {
             System.out.println("Capitulo " + chapter + " baixado com sucesso!");
             return true;
