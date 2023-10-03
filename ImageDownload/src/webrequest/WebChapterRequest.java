3d3346b4d20fe5b757d250a62bd9a6eec38e19cc
fiddelis <lucas.ruan@ges.inatel.br>
2023-10-03 11:57:47 -0300

    Html do capitulo

--- ImageDownload/src/webrequest/WebChapterRequest.java
+++ ImageDownload/src/webrequest/WebChapterRequest.java
@@ -11,34 +11,37 @@ import java.net.URL;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class HtmlRequest {
-
+public class WebChapterRequest {
     public String ChapterRequest(String imageUrl) throws IOException {
         URL url = new URL(imageUrl);
-        try {
-            URLConnection connection = url.openConnection();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            String content;
-            StringBuilder html = new StringBuilder();
+        URLConnection connection = url.openConnection();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String content;
+        StringBuilder html = new StringBuilder();
 
-            while ((content = reader.readLine()) != null) {
-                html.append(content);
-            }
-            reader.close();
-            return regex(html);
-
-        } catch (FileNotFoundException e) {
-            throw e;
+        while ((content = reader.readLine()) != null) {
+            html.append(content);
         }
+        reader.close();
+        System.out.println(html);
+        return regex(html);
+
     }
 
     private String regex(StringBuilder html) {
         Pattern p = Pattern.compile("(https:\\\\/\\\\/)+(\\S)+(.jpg)");
         Matcher m = p.matcher(html);
+
         if (m.find()) {
             return m.group().replace("\\", "");
-        } else {
-            return "not found";
         }
+
+        p = Pattern.compile("(https:\\\\/\\\\/)+(\\S)+(.png)");
+        m = p.matcher(html);
+        if (m.find()) {
+            return m.group().replace("\\", "");
+        }
+
+        return "not found";
     }
 }
