219a3e6175157de7daf527fa473b81a3c6440308
fiddelis <lucas.ruan@ges.inatel.br>
2023-10-03 11:59:52 -0300

    Conexão com o Url das Imagens

--- ImageDownload/src/webrequest/WebPageRequest.java
+++ ImageDownload/src/webrequest/WebPageRequest.java
@@ -1,2 +1,27 @@
-package webrequest;public class HtmlPageRequest {
+package webrequest;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class WebPageRequest {
+    public InputStream inputRequest(String imageUrl) throws IOException {
+        InputStream inputStream;
+        URL url;
+
+        try {
+            url = new URL(imageUrl);
+            URLConnection connection = url.openConnection();
+            inputStream = connection.getInputStream();
+        } catch (IOException e) {   // caso não consiga conexão com o link .jpg
+            imageUrl = imageUrl.substring(0, imageUrl.length() - 4) + ".png";    // altera o formato da imagem
+
+            url = new URL(imageUrl);
+            URLConnection connection = url.openConnection();
+            inputStream = connection.getInputStream();
+        }
+
+        return inputStream;
+    }
 }
